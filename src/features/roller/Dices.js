24dec45b9352014ca0d1c769076ab9db38aff3f8
Ceithir <ceathramh@gmail.com>
2020-10-13 20:56:04 +0200

    Add more information about rerolls

--- src/features/roller/Dices.js
+++ src/features/roller/Dices.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { List } from "antd";
+import { List, Tooltip } from "antd";
 import styles from "./Dices.module.css";
 import classNames from "classnames";
 import Dice from "./Dice";
@@ -24,20 +24,48 @@ const Dices = ({ dices }) => {
     <List
       grid={{ gutter: 0, column: 10, xs: 5 }}
       dataSource={listData}
-      renderItem={({ key, dice, selectable, selected, toggle, disabled }) => (
-        <List.Item
-          key={key}
-          className={classNames(styles.dice, {
-            [styles.selectable]: selectable,
-            [styles.selected]: selected,
-            [styles.unselectable]: disabled,
-            [styles.rerolled]: dice.status === "rerolled",
-          })}
-          onClick={selectable ? toggle : undefined}
-        >
-          <Dice dice={dice} />
-        </List.Item>
-      )}
+      renderItem={({ key, dice, selectable, selected, toggle, disabled }) => {
+        const modifier = dice?.metadata?.modifier;
+        const rerolled = dice.status === "rerolled";
+        const fromReroll = modifier && dice.status !== "rerolled";
+
+        const tooltipText = () => {
+          if (rerolled) {
+            if (modifier === "adversity") {
+              return "Rerolled due to Adversity";
+            }
+            if (modifier === "distinction") {
+              return "Rerolled thanks to Distinction";
+            }
+          }
+
+          if (fromReroll) {
+            return "This dice is the result of a reroll";
+          }
+
+          return;
+        };
+
+        return (
+          <List.Item
+            key={key}
+            className={classNames(styles.dice, {
+              [styles.selectable]: selectable,
+              [styles.selected]: selected,
+              [styles.unselectable]: disabled,
+              [styles.rerolled]: rerolled,
+              [styles["from-reroll"]]: fromReroll,
+            })}
+            onClick={selectable ? toggle : undefined}
+          >
+            <Tooltip title={tooltipText()}>
+              <div>
+                <Dice dice={dice} />
+              </div>
+            </Tooltip>
+          </List.Item>
+        );
+      }}
     />
   );
 };
