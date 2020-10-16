5d813ab87b2da0f3046c49ecfd2265db3a16d7cf
Ceithir <ceathramh@gmail.com>
2020-10-16 07:50:16 +0200

    Extend card to whole width

--- src/features/roller/DicesBox.js
+++ src/features/roller/DicesBox.js
@@ -7,11 +7,13 @@ const { Paragraph, Title } = Typography;
 
 const DicesBox = ({ dices, text, footer, title, theme }) => {
   return (
-    <Card className="boxed">
-      {title && <Title level={3}>{title}</Title>}
-      {text && <Paragraph>{text}</Paragraph>}
-      <Dices dices={dices} theme={theme} />
-      {footer && <div className={styles.footer}>{footer}</div>}
+    <Card>
+      <div className="boxed">
+        {title && <Title level={3}>{title}</Title>}
+        {text && <Paragraph>{text}</Paragraph>}
+        <Dices dices={dices} theme={theme} />
+        {footer && <div className={styles.footer}>{footer}</div>}
+      </div>
     </Card>
   );
 };
