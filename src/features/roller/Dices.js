b6995bc780d36c1eefc8c93e7f92dfff413d8970
Ceithir <ceathramh@gmail.com>
2020-11-10 19:04:14 +0100

    Remove useless card

--- src/features/roller/Dices.js
+++ src/features/roller/Dices.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { List, Card } from "antd";
+import { List } from "antd";
 import styles from "./Dices.module.css";
 import classNames from "classnames";
 import Dice from "./Dice";
@@ -21,34 +21,33 @@ const Dices = ({ dices, theme }) => {
   });
 
   return (
-    <Card className={styles.dices}>
-      <List
-        grid={{ gutter: 0, column: 7, xs: 4 }}
-        dataSource={listData}
-        renderItem={({ key, dice, selectable, selected, toggle, disabled }) => {
-          const modifier = dice?.metadata?.modifier;
-          const rerolled = dice.status === "rerolled";
-          const fromReroll = modifier && dice.status !== "rerolled";
+    <List
+      className={styles.dices}
+      grid={{ gutter: 0, column: 7, xs: 4 }}
+      dataSource={listData}
+      renderItem={({ key, dice, selectable, selected, toggle, disabled }) => {
+        const modifier = dice?.metadata?.modifier;
+        const rerolled = dice.status === "rerolled";
+        const fromReroll = modifier && dice.status !== "rerolled";
 
-          return (
-            <List.Item
-              key={key}
-              className={classNames(styles.dice, {
-                [styles.selectable]: selectable,
-                [styles.selected]: selected,
-                [styles.unselectable]: disabled,
-                [styles.rerolled]: rerolled,
-                [styles["from-reroll"]]: fromReroll,
-                [styles[`theme-${theme}`]]: !!theme,
-              })}
-              onClick={selectable ? toggle : undefined}
-            >
-              <Dice dice={dice} />
-            </List.Item>
-          );
-        }}
-      />
-    </Card>
+        return (
+          <List.Item
+            key={key}
+            className={classNames(styles.dice, {
+              [styles.selectable]: selectable,
+              [styles.selected]: selected,
+              [styles.unselectable]: disabled,
+              [styles.rerolled]: rerolled,
+              [styles["from-reroll"]]: fromReroll,
+              [styles[`theme-${theme}`]]: !!theme,
+            })}
+            onClick={selectable ? toggle : undefined}
+          >
+            <Dice dice={dice} />
+          </List.Item>
+        );
+      }}
+    />
   );
 };
 
