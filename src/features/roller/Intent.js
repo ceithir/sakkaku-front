eef0d915622dbfd7f25d94ba24598a52a719a837
Ceithir <ceathramh@gmail.com>
2020-11-13 11:16:35 +0100

    Remove useless card

--- src/features/roller/Intent.js
+++ src/features/roller/Intent.js
@@ -7,7 +7,6 @@ import {
   Switch,
   Divider,
   AutoComplete,
-  Card,
 } from "antd";
 import styles from "./Intent.module.css";
 import NextButton from "./NextButton";
@@ -108,56 +107,54 @@ const Intent = ({ onFinish, values, onComplete }) => {
   };
 
   return (
-    <Card>
-      <Form
-        className={`boxed ${styles.form}`}
-        {...layout}
-        initialValues={values}
-        onFinish={wrappedOnFinish}
+    <Form
+      className={`boxed ${styles.form}`}
+      {...layout}
+      initialValues={values}
+      onFinish={wrappedOnFinish}
+    >
+      <Form.Item label="Campaign" name="campaign" rules={defaultRules}>
+        <AutoComplete options={arrayToAutoCompleteOptions(campaigns)} />
+      </Form.Item>
+      <Form.Item label="Character" name="character" rules={defaultRules}>
+        <AutoComplete options={arrayToAutoCompleteOptions(characters)} />
+      </Form.Item>
+      <Form.Item label="TN" name="tn" rules={defaultRules}>
+        <InputNumber min={1} />
+      </Form.Item>
+      <Form.Item label="Description" name="description" rules={defaultRules}>
+        <TextArea />
+      </Form.Item>
+      <Divider />
+      <Form.Item label="Ring" name="ring" rules={defaultRules}>
+        <InputNumber min={1} max={5} />
+      </Form.Item>
+      <Form.Item label="Skill" name="skill" rules={defaultRules}>
+        <InputNumber min={0} max={5} />
+      </Form.Item>
+      <Divider />
+      <Form.Item label="Void?" name="void" valuePropName="checked">
+        <Switch />
+      </Form.Item>
+      <Form.Item label="Advantage / Disadvantage" name="modifier">
+        <Radio.Group>
+          <Radio.Button value="">None</Radio.Button>
+          <Radio.Button value="distinction">Distinction</Radio.Button>
+          <Radio.Button value="adversity">Adversity</Radio.Button>
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item
+        label="Compromised?"
+        name="compromised"
+        valuePropName="checked"
       >
-        <Form.Item label="Campaign" name="campaign" rules={defaultRules}>
-          <AutoComplete options={arrayToAutoCompleteOptions(campaigns)} />
-        </Form.Item>
-        <Form.Item label="Character" name="character" rules={defaultRules}>
-          <AutoComplete options={arrayToAutoCompleteOptions(characters)} />
-        </Form.Item>
-        <Form.Item label="TN" name="tn" rules={defaultRules}>
-          <InputNumber min={1} />
-        </Form.Item>
-        <Form.Item label="Description" name="description" rules={defaultRules}>
-          <TextArea />
-        </Form.Item>
-        <Divider />
-        <Form.Item label="Ring" name="ring" rules={defaultRules}>
-          <InputNumber min={1} max={5} />
-        </Form.Item>
-        <Form.Item label="Skill" name="skill" rules={defaultRules}>
-          <InputNumber min={0} max={5} />
-        </Form.Item>
-        <Divider />
-        <Form.Item label="Void?" name="void" valuePropName="checked">
-          <Switch />
-        </Form.Item>
-        <Form.Item label="Advantage / Disadvantage" name="modifier">
-          <Radio.Group>
-            <Radio.Button value="">None</Radio.Button>
-            <Radio.Button value="distinction">Distinction</Radio.Button>
-            <Radio.Button value="adversity">Adversity</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item
-          label="Compromised?"
-          name="compromised"
-          valuePropName="checked"
-        >
-          <Switch />
-        </Form.Item>
-        <Divider />
-        <Form.Item {...tailLayout}>
-          <NextButton htmlType="submit">Roll</NextButton>
-        </Form.Item>
-      </Form>
-    </Card>
+        <Switch />
+      </Form.Item>
+      <Divider />
+      <Form.Item {...tailLayout}>
+        <NextButton htmlType="submit">Roll</NextButton>
+      </Form.Item>
+    </Form>
   );
 };
 
