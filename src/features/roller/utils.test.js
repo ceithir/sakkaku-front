b7e5be457ffe8c18337c6596c1771fc8dce580d5
Ceithir <ceathramh@gmail.com>
2020-12-07 09:24:57 +0100

    Add test for legacy support

--- src/features/roller/utils.test.js
+++ src/features/roller/utils.test.js
@@ -1,172 +1,283 @@
 import { replaceRerollsOfType } from "./utils";
 
-const roll = {
-  dices: [
-    {
-      type: "ring",
-      value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
-      status: "rerolled",
-      metadata: { end: "shadow", modifier: "shadow" },
-    },
-    {
-      type: "ring",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
-      status: "dropped",
-      metadata: [],
-    },
-    {
-      type: "ring",
-      value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
-      status: "rerolled",
-      metadata: { end: "distinction", modifier: "distinction" },
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
-      status: "kept",
-      metadata: [],
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
-      status: "rerolled",
-      metadata: { end: "distinction", modifier: "distinction" },
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 1 },
-      status: "dropped",
-      metadata: [],
-    },
-    {
-      type: "ring",
-      value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
-      status: "rerolled",
-      metadata: { end: "shadow", source: "distinction", modifier: "shadow" },
-    },
-    {
-      type: "skill",
-      value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
-      status: "rerolled",
-      metadata: { end: "shadow", source: "distinction", modifier: "shadow" },
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 0, explosion: 0, opportunity: 0 },
-      status: "kept",
-      metadata: { source: "shadow", modifier: "shadow" },
-    },
-    {
-      type: "ring",
-      value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
-      status: "dropped",
-      metadata: { source: "shadow", modifier: "shadow" },
-    },
-    {
-      type: "ring",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
-      status: "kept",
-      metadata: { source: "shadow", modifier: "shadow" },
-    },
-  ],
-  metadata: { rerolls: ["distinction", "shadow"] },
-  parameters: {
-    tn: 3,
-    ring: 3,
-    skill: 3,
-    modifiers: ["distinction", "shadow"],
-  },
-};
-const dices = roll.dices;
-const basePool = 6;
+describe("replaceRerollsOfType", () => {
+  describe("current", () => {
+    const roll = {
+      dices: [
+        {
+          type: "ring",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
+          status: "rerolled",
+          metadata: { end: "shadow" },
+        },
+        {
+          type: "ring",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "dropped",
+          metadata: [],
+        },
+        {
+          type: "ring",
+          value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
+          status: "rerolled",
+          metadata: { end: "distinction" },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "kept",
+          metadata: [],
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
+          status: "rerolled",
+          metadata: { end: "distinction" },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 1 },
+          status: "dropped",
+          metadata: [],
+        },
+        {
+          type: "ring",
+          value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
+          status: "rerolled",
+          metadata: {
+            end: "shadow",
+            source: "distinction",
+          },
+        },
+        {
+          type: "skill",
+          value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
+          status: "rerolled",
+          metadata: {
+            end: "shadow",
+            source: "distinction",
+          },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 0 },
+          status: "kept",
+          metadata: { source: "shadow" },
+        },
+        {
+          type: "ring",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
+          status: "dropped",
+          metadata: { source: "shadow" },
+        },
+        {
+          type: "ring",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "kept",
+          metadata: { source: "shadow" },
+        },
+      ],
+      metadata: { rerolls: ["distinction", "shadow"] },
+      parameters: {
+        tn: 3,
+        ring: 3,
+        skill: 3,
+        modifiers: ["distinction", "shadow"],
+      },
+    };
+    const dices = roll.dices;
+    const basePool = roll.parameters.ring + roll.parameters.skill;
 
-test("filter first depth of reroll properly", () => {
-  expect(
-    replaceRerollsOfType({ dices, rerollType: "distinction", basePool })
-  ).toStrictEqual([
-    {
-      type: "ring",
-      value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
-      status: "rerolled",
-      metadata: { end: "shadow", modifier: "shadow" },
-    },
-    {
-      type: "ring",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
-      status: "dropped",
-      metadata: [],
-    },
-    {
-      type: "ring",
-      value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
-      status: "rerolled",
-      metadata: { end: "shadow", source: "distinction", modifier: "shadow" },
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
-      status: "kept",
-      metadata: [],
-    },
-    {
-      type: "skill",
-      value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
-      status: "rerolled",
-      metadata: { end: "shadow", source: "distinction", modifier: "shadow" },
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 1 },
-      status: "dropped",
-      metadata: [],
-    },
-  ]);
-});
+    test("filter first depth of reroll properly", () => {
+      expect(
+        replaceRerollsOfType({ dices, rerollType: "distinction", basePool })
+      ).toStrictEqual([
+        {
+          type: "ring",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
+          status: "rerolled",
+          metadata: { end: "shadow" },
+        },
+        {
+          type: "ring",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "dropped",
+          metadata: [],
+        },
+        {
+          type: "ring",
+          value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
+          status: "rerolled",
+          metadata: {
+            end: "shadow",
+            source: "distinction",
+          },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "kept",
+          metadata: [],
+        },
+        {
+          type: "skill",
+          value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
+          status: "rerolled",
+          metadata: {
+            end: "shadow",
+            source: "distinction",
+          },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 1 },
+          status: "dropped",
+          metadata: [],
+        },
+      ]);
+    });
 
-test("filter second depth of reroll properly", () => {
-  expect(
-    replaceRerollsOfType({
-      dices,
-      rerollType: "shadow",
-      basePool,
-      previousRerollTypes: ["distinction"],
-    })
-  ).toStrictEqual([
-    {
-      type: "ring",
-      value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
-      status: "dropped",
-      metadata: { source: "shadow", modifier: "shadow" },
-    },
-    {
-      type: "ring",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
-      status: "dropped",
-      metadata: [],
-    },
-    {
-      type: "ring",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
-      status: "kept",
-      metadata: { source: "shadow", modifier: "shadow" },
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
-      status: "kept",
-      metadata: [],
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 0, explosion: 0, opportunity: 0 },
-      status: "kept",
-      metadata: { source: "shadow", modifier: "shadow" },
-    },
-    {
-      type: "skill",
-      value: { strife: 0, success: 1, explosion: 0, opportunity: 1 },
-      status: "dropped",
-      metadata: [],
-    },
-  ]);
+    test("filter second depth of reroll properly", () => {
+      expect(
+        replaceRerollsOfType({
+          dices,
+          rerollType: "shadow",
+          basePool,
+          previousRerollTypes: ["distinction"],
+        })
+      ).toStrictEqual([
+        {
+          type: "ring",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
+          status: "dropped",
+          metadata: { source: "shadow" },
+        },
+        {
+          type: "ring",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "dropped",
+          metadata: [],
+        },
+        {
+          type: "ring",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "kept",
+          metadata: { source: "shadow" },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "kept",
+          metadata: [],
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 0 },
+          status: "kept",
+          metadata: { source: "shadow" },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 1 },
+          status: "dropped",
+          metadata: [],
+        },
+      ]);
+    });
+  });
+
+  describe("legacy", () => {
+    const roll = {
+      dices: [
+        {
+          type: "ring",
+          value: { strife: 1, success: 0, explosion: 1, opportunity: 0 },
+          status: "dropped",
+          metadata: [],
+        },
+        {
+          type: "ring",
+          value: { strife: 1, success: 0, explosion: 1, opportunity: 0 },
+          status: "kept",
+          metadata: [],
+        },
+        {
+          type: "skill",
+          value: { strife: 1, success: 0, explosion: 1, opportunity: 0 },
+          status: "rerolled",
+          metadata: { modifier: "distinction" },
+        },
+        {
+          type: "skill",
+          value: { strife: 1, success: 0, explosion: 1, opportunity: 0 },
+          status: "rerolled",
+          metadata: { modifier: "distinction" },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "dropped",
+          metadata: [],
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 1 },
+          status: "kept",
+          metadata: { modifier: "distinction" },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
+          status: "dropped",
+          metadata: { modifier: "distinction" },
+        },
+        {
+          type: "ring",
+          value: { strife: 1, success: 1, explosion: 0, opportunity: 0 },
+          status: "kept",
+          metadata: [],
+        },
+      ],
+      metadata: { rerolls: ["distinction"] },
+      parameters: { tn: 3, ring: 2, skill: 3, modifiers: ["distinction"] },
+    };
+    const dices = roll.dices;
+
+    test("filter simple legacy roll properly", () => {
+      expect(
+        replaceRerollsOfType({ dices, rerollType: "distinction", basePool: 5 })
+      ).toStrictEqual([
+        {
+          type: "ring",
+          value: { strife: 1, success: 0, explosion: 1, opportunity: 0 },
+          status: "dropped",
+          metadata: [],
+        },
+        {
+          type: "ring",
+          value: { strife: 1, success: 0, explosion: 1, opportunity: 0 },
+          status: "kept",
+          metadata: [],
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 1 },
+          status: "kept",
+          metadata: { modifier: "distinction" },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 0, explosion: 0, opportunity: 1 },
+          status: "dropped",
+          metadata: { modifier: "distinction" },
+        },
+        {
+          type: "skill",
+          value: { strife: 0, success: 1, explosion: 0, opportunity: 0 },
+          status: "dropped",
+          metadata: [],
+        },
+      ]);
+    });
+  });
 });
