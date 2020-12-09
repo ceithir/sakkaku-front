706cc7b42913c1c692142ea6ba3a1955d538f7a0
Ceithir <ceathramh@gmail.com>
2020-12-09 20:59:37 +0100

    Fix button floating due to ant design update

--- src/features/roller/NextButton.js
+++ src/features/roller/NextButton.js
@@ -8,14 +8,16 @@ const NextButton = ({ children, disabled, ...props }) => {
   const loading = useSelector(selectLoading);
 
   return (
-    <Button
-      type="primary"
-      className={styles.button}
-      disabled={disabled || loading}
-      {...props}
-    >
-      {children}
-    </Button>
+    <div className={styles.layout}>
+      <Button
+        type="primary"
+        className={styles.button}
+        disabled={disabled || loading}
+        {...props}
+      >
+        {children}
+      </Button>
+    </div>
   );
 };
 
