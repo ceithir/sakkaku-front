83e202fdf3a76ddb48b70ec35282ff18063be782
Ceithir <ceathramh@gmail.com>
2021-02-13 17:17:39 +0100

    Fix Sailor slightly rushed display

--- src/features/roller/reroll/Sailor.js
+++ src/features/roller/reroll/Sailor.js
@@ -1,26 +1,40 @@
 import React from "react";
 import NextButton from "../NextButton";
 import Ability from "./Ability";
-import { Typography, Card } from "antd";
-
-const { Paragraph } = Typography;
+import DicesBox from "../DicesBox";
+import { longname } from "../data/abilities";
+import { replaceRerolls } from "../utils";
 
 const Sailor = ({ dices, onFinish, basePool, rerollTypes, compromised }) => {
+  const title = longname("sailor");
+
   if (compromised) {
     return (
-      <Card bordered={false}>
-        <div className="boxed">
-          <Paragraph>
-            {"You may not use your School Ability as you are compromised."}
-          </Paragraph>
+      <DicesBox
+        title={title}
+        text={"You may not use your School Ability as you are compromised."}
+        dices={replaceRerolls({
+          dices: dices.map((dice) => {
+            return {
+              ...dice,
+              selectable: false,
+              disabled: true,
+            };
+          }),
+          basePool,
+          rerollTypes,
+        })}
+        footer={
           <NextButton onClick={() => onFinish([])}>{"Continue"}</NextButton>
-        </div>
-      </Card>
+        }
+        theme="reroll"
+      />
     );
   }
 
   return (
     <Ability
+      title={title}
       text={
         "You may receive a number of strife up to your school rank to reroll that many rolled dice."
       }
