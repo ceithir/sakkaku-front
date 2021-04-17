ae48db6da3e4d1c394722ea2e68a242eef9f50ea
Ceithir <ceathramh@gmail.com>
2021-04-17 15:37:06 +0200

    Enable LESS CSS modules

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,51 @@
+// Ref: https://github.com/DocSpring/craco-less/issues/45#issuecomment-692571756
+
 const CracoLessPlugin = require("craco-less");
+
+const modifiedTheme = {};
 
 module.exports = {
   plugins: [
+    // This plugin takes care of the .less files
     {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
           lessOptions: {
+            modifyVars: modifiedTheme,
             javascriptEnabled: true,
           },
+        },
+      },
+    },
+    // This plugin take scare of the .module.less files
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        modifyLessRule: function (lessRule, _context) {
+          lessRule.test = /\.(less)$/;
+          lessRule.use = [
+            {
+              loader: "style-loader",
+            },
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+              },
+            },
+            {
+              loader: "less-loader",
+              options: {
+                lessOptions: {
+                  modifyVars: modifiedTheme,
+                  javascriptEnabled: true,
+                },
+              },
+            },
+          ];
+          lessRule.exclude = /node_modules/;
+          return lessRule;
         },
       },
     },
