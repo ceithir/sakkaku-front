2765a1c832ba6ecfa9d87e823bcaaa904d9fa1de
Ceithir <ceathramh@gmail.com>
2021-05-18 09:30:43 +0200

    Add compromised

--- src/features/probabilities/Calculator.js
+++ src/features/probabilities/Calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, InputNumber, Typography } from "antd";
+import { Form, InputNumber, Typography, Switch } from "antd";
 import { cumulativeSuccess } from "./maths";
 import styles from "./Calculator.module.less";
 
@@ -9,14 +9,16 @@ const Calculator = () => {
   const [form] = Form.useForm();
   const initialValues = { ring: 3, skill: 1, tn: 3 };
   const [values, setValues] = useState(initialValues);
+  const [compromised, setComprised] = useState(false);
 
   return (
     <Form
       layout={"inline"}
       form={form}
-      initialValues={initialValues}
-      onValuesChange={(_, values) => {
-        setValues(values);
+      initialValues={{ ...initialValues, compromised: false }}
+      onValuesChange={(_, { ring, skill, tn, compromised }) => {
+        setValues({ ring, skill, tn });
+        setComprised(compromised);
       }}
       className={styles.form}
     >
@@ -29,14 +31,25 @@ const Calculator = () => {
       <Form.Item label="TN" name="tn">
         <InputNumber min={1} max={10} />
       </Form.Item>
+      <Form.Item
+        label={"Compromised?"}
+        name="compromised"
+        valuePropName="checked"
+      >
+        <Switch />
+      </Form.Item>
       <output>
         <Paragraph>
           <Text>
-            {`Chances to achieve TN, by taking as much strife as necessary, ignoring rerolls, alterations and other modifiers: `}
+            {compromised
+              ? `Chances to achieve TN, by taking no strife at all, ignoring rerolls, alterations and other modifiers: `
+              : `Chances to achieve TN, by taking as much strife as necessary, ignoring rerolls, alterations and other modifiers: `}
           </Text>
-          <Text strong>{`${(cumulativeSuccess(values) * 100).toFixed(
-            2
-          )}%`}</Text>
+          <Text strong>{`${(
+            Math.abs(
+              cumulativeSuccess({ ...values, options: { compromised } })
+            ) * 100
+          ).toFixed(2)}%`}</Text>
           <Text>{`.`}</Text>
         </Paragraph>
       </output>
