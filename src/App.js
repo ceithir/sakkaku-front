0f57ffc859527f97f9bc4c929f31413747f28183
Ceithir <ceathramh@gmail.com>
2021-06-25 21:19:10 +0200

    Lazy load routes
    https://reactjs.org/docs/code-splitting.html#route-based-code-splitting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense, lazy } from "react";
 import "./App.less";
-import Roller from "./features/roller";
 import Layout from "./features/navigation/Layout";
 import { getOnServer } from "./server";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import List from "./features/browse/List";
-import IdentifiedRoll from "./features/roller/IdentifiedRoll";
 import { useDispatch } from "react-redux";
 import { setUser } from "./features/user/reducer";
-import HeritageRoll from "./features/heritage/Roll";
-import HeritageRollLoader from "./features/heritage/RollLoader";
-import HeritageListLoader from "./features/heritage/ListLoader";
-import Calculator from "./features/probabilities/Calculator";
+import Loader from "./features/navigation/Loader";
+
+const Roller = lazy(() => import("./features/roller"));
+const List = lazy(() => import("./features/browse/List"));
+const IdentifiedRoll = lazy(() => import("./features/roller/IdentifiedRoll"));
+const HeritageRoll = lazy(() => import("./features/heritage/Roll"));
+const HeritageRollLoader = lazy(() => import("./features/heritage/RollLoader"));
+const HeritageListLoader = lazy(() => import("./features/heritage/ListLoader"));
+const Calculator = lazy(() => import("./features/probabilities/Calculator"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -28,29 +30,31 @@ const App = () => {
   return (
     <Router>
       <Layout>
-        <Switch>
-          <Route path="/probabilities">
-            <Calculator />
-          </Route>
-          <Route path="/heritage/list">
-            <HeritageListLoader />
-          </Route>
-          <Route path="/heritage/:uuid">
-            <HeritageRollLoader />
-          </Route>
-          <Route path="/heritage">
-            <HeritageRoll />
-          </Route>
-          <Route path="/rolls/:id">
-            <IdentifiedRoll />
-          </Route>
-          <Route path="/rolls">
-            <List />
-          </Route>
-          <Route path="/">
-            <Roller />
-          </Route>
-        </Switch>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            <Route path="/probabilities">
+              <Calculator />
+            </Route>
+            <Route path="/heritage/list">
+              <HeritageListLoader />
+            </Route>
+            <Route path="/heritage/:uuid">
+              <HeritageRollLoader />
+            </Route>
+            <Route path="/heritage">
+              <HeritageRoll />
+            </Route>
+            <Route path="/rolls/:id">
+              <IdentifiedRoll />
+            </Route>
+            <Route path="/rolls">
+              <List />
+            </Route>
+            <Route path="/">
+              <Roller />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
     </Router>
   );
