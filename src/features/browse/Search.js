85209e05dc98ebed075446d78e3b137e5a34730e
Ceithir <ceathramh@gmail.com>
2021-07-22 16:41:27 +0200

    Fix form display on mobile

--- src/features/browse/Search.js
+++ src/features/browse/Search.js
@@ -35,29 +35,33 @@ const StaticSearch = ({ initialValues, onFinish, campaigns, characters }) => {
   return (
     <div className={styles.container}>
       <Title level={2}>{`Search`}</Title>
-      <Form onFinish={onFinish} layout="inline" initialValues={initialValues}>
-        <Form.Item label={`Campaign`} name="campaign">
-          <AutoComplete options={arrayToAutoCompleteOptions(campaigns)} />
-        </Form.Item>
-        <Form.Item label={`Character`} name="character">
-          <AutoComplete options={arrayToAutoCompleteOptions(characters)} />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            {`Submit`}
-          </Button>
-        </Form.Item>
+      <Form onFinish={onFinish} initialValues={initialValues}>
+        <fieldset>
+          <Form.Item label={`Campaign`} name="campaign">
+            <AutoComplete options={arrayToAutoCompleteOptions(campaigns)} />
+          </Form.Item>
+          <Form.Item label={`Character`} name="character">
+            <AutoComplete options={arrayToAutoCompleteOptions(characters)} />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              {`Submit`}
+            </Button>
+          </Form.Item>
+        </fieldset>
         <Collapse ghost>
           <Panel header="Advanced">
-            <Form.Item label={`Type`} name="type">
-              <Select
-                options={[
-                  { value: "FFG-L5R", label: `FFG L5R Check` },
-                  { value: "FFG-L5R-Heritage", label: `FFG L5R Heritage` },
-                ]}
-                allowClear={true}
-              />
-            </Form.Item>
+            <fieldset>
+              <Form.Item label={`Type`} name="type">
+                <Select
+                  options={[
+                    { value: "FFG-L5R", label: `FFG L5R Check` },
+                    { value: "FFG-L5R-Heritage", label: `FFG L5R Heritage` },
+                  ]}
+                  allowClear={true}
+                />
+              </Form.Item>
+            </fieldset>
           </Panel>
         </Collapse>
       </Form>
