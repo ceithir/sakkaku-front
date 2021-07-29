8cf0ac39d4ea70efa738e94d019520e034f41dcb
Ceithir <ceathramh@gmail.com>
2021-07-29 17:54:34 +0200

    Store config locally

--- src/features/roller/config/reducer.js
+++ src/features/roller/config/reducer.js
@@ -1,5 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const namespace = "HMRpb9OPyoEmg3Oqa4Xi85KdUWOARlFX";
+const store = (namespace) => {
+  const fullKey = (key) => `${namespace}-${key}`;
+
+  return {
+    get: (key, fallback) => {
+      const item = window.localStorage.getItem(fullKey(key));
+      if (item === null) {
+        return fallback;
+      }
+      return item;
+    },
+    set: (key, value) => {
+      window.localStorage.setItem(fullKey(key), value);
+    },
+  };
+};
+const { get, set } = store(namespace);
+
 const initialState = {
   mode: "semiauto",
   displayMode: "compact",
@@ -7,11 +26,18 @@ const initialState = {
 
 const slice = createSlice({
   name: "rollConfig",
-  initialState,
+  initialState: (() => {
+    let obj = {};
+    for (const key in initialState) {
+      obj[key] = get(key, initialState[key]);
+    }
+    return obj;
+  })(),
   reducers: {
     updateConfig: (state, { payload }) => {
       for (const key in payload) {
         state[key] = payload[key];
+        set(key, payload[key]);
       }
     },
   },
