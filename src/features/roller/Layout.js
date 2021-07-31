7e44f6d0d1c94d7b2ce668ce4005dac115b40c65
Ceithir <ceathramh@gmail.com>
2021-07-31 19:17:06 +0200

    Remove overkill framework variable change

--- src/features/roller/Layout.js
+++ src/features/roller/Layout.js
@@ -3,13 +3,14 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../user/reducer";
 import AnonymousAlert from "../../AnonymousAlert";
 import Title from "../display/Title";
+import styles from "./Layout.module.less";
 
 export const CommonLayout = ({ children }) => {
   return (
-    <>
+    <div className={styles.layout}>
       <Title>{`Legend of the Five Rings – Check Roll`}</Title>
       <>{children}</>
-    </>
+    </div>
   );
 };
 
