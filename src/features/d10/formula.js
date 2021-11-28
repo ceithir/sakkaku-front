63ff4e37ee4437f81eac8c28cc8112fae89d1fe3
Ceithir <ceathramh@gmail.com>
2021-11-28 15:19:34 +0100

    Fix warnings about unnecessary characters

--- src/features/d10/formula.js
+++ src/features/d10/formula.js
@@ -4,9 +4,7 @@ export const parse = (str) => {
   }
 
   const s = str.replace(/\s+/g, "");
-  const matches = s.match(
-    /^([0-9]+)k([0-9]+)((\+|\-)([0-9]+k[0-9]+|[0-9]+))*$/
-  );
+  const matches = s.match(/^([0-9]+)k([0-9]+)((\+|-)([0-9]+k[0-9]+|[0-9]+))*$/);
 
   if (!matches) {
     return false;
@@ -18,25 +16,24 @@ export const parse = (str) => {
   const plusXKeepYGroups = s.match(/\+[0-9]+k[0-9]+/g);
   if (!!plusXKeepYGroups) {
     plusXKeepYGroups.forEach((plusXKeepY) => {
-      const [_, extraRoll, extraKeep] = plusXKeepY.match(
+      const [, extraRoll, extraKeep] = plusXKeepY.match(
         /^\+([0-9]+)k([0-9]+)$/
       );
       roll += parseInt(extraRoll);
       keep += parseInt(extraKeep);
     });
   }
-  const minusXKeepYGroups = s.match(/\-[0-9]+k[0-9]+/g);
+  const minusXKeepYGroups = s.match(/-[0-9]+k[0-9]+/g);
   if (!!minusXKeepYGroups) {
     minusXKeepYGroups.forEach((minusXKeepY) => {
-      const [_, extraRoll, extraKeep] = minusXKeepY.match(
-        /^\-([0-9]+)k([0-9]+)$/
-      );
+      const [, extraRoll, extraKeep] =
+        minusXKeepY.match(/^-([0-9]+)k([0-9]+)$/);
       roll -= parseInt(extraRoll);
       keep -= parseInt(extraKeep);
     });
   }
 
-  const modifiers = s.match(/(\+|\-)[0-9]+(?!k)/g);
+  const modifiers = s.match(/(\+|-)[0-9]+(?!k)/g);
   if (!!modifiers) {
     modifiers.forEach((mod) => {
       modifier += parseInt(mod);
