bd281566ed3a2b30850314deffd1012d55b92da2
Ceithir <ceathramh@gmail.com>
2022-04-10 16:27:29 +0200

    Better error message
    Well, bigger at least

--- src/DefaultErrorMessage.js
+++ src/DefaultErrorMessage.js
@@ -4,9 +4,16 @@ import { Alert } from "antd";
 const DefaultErrorMessage = () => {
   return (
     <Alert
-      message="Something bad happened. Try reloading the page. If things are still broken, please contact administrator through the link at the bottom of the page."
+      message={`Something bad happened.`}
+      description={
+        <>
+          <>{`Possibly, just a minor network hiccup at the worst timing. Possibly, a devious bug in Sakkaku's code. I don't know, I'm just a generic error message.`}</>
+          <br />
+          <>{`Try reloading the page. If things are still broken, please contact the administrator using the link at the very bottom of the page.`}</>
+        </>
+      }
       type="error"
-      banner={true}
+      showIcon={true}
     />
   );
 };
