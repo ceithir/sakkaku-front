ba7c9475b404091741d5e4f6898b446f2ab51cda
Ceithir <ceathramh@gmail.com>
2022-05-05 18:52:58 +0200

    Update footer

--- src/features/navigation/Layout.js
+++ src/features/navigation/Layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Layout } from "antd";
 import styles from "./Layout.module.less";
 import Menu from "./Menu";
-import ExternalLink from "./ExternalLink";
 
 const { Header, Content, Footer } = Layout;
 
@@ -14,13 +13,9 @@ const CustomLayout = ({ children }) => {
       </Header>
       <Content>{children}</Content>
       <Footer className={styles.footer}>
-        {`A dice roller for the `}
-        <ExternalLink href="https://www.fantasyflightgames.com/en/legend-of-the-five-rings-roleplaying-game/">
-          {`Legend of the Five Rings Roleplaying Game (5th edition)`}
-        </ExternalLink>
-        {` – ©FFG for all quoted texts and images`}
+        {`A dice roller with a public roll history, for easy use in play-by-post.`}
         <br />
-        {`This website is not affiliated in any way with Fantasy Flight Games or EDGE Studio.`}
+        {`This website is not affiliated in any way with any of the licenses it offers rollers for.`}
         <br />
         {`For any issue or suggestion: `}
         <a href="mailto:contact.sakkaku@gmail.com">{`contact.sakkaku@gmail.com`}</a>
